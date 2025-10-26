1ed44cb41c94a2a25068113122a4f70bb133c93d
Aaron Spiteri <azzmosphere@gmail.com>
2025-10-26 16:57:03 +1100

    Corrected headers. that should be exposed

---
+++ src/rr_subscriber_batt_state_impl.cpp
@@ -0,0 +1,14 @@
+#include "rr_common_plugins/rr_batt_state_subscriber_impl.hpp"
+
+using namespace rr_common_plugins;
+
+/**
+ * @fn callback
+ * 
+ * callback for battery state.
+ */
+void RrSubscriberBattStateImpl::callback(const sensor_msgs::msg::BatteryState::SharedPtr msg)
+{
+    RCLCPP_DEBUG(logger_, "received message battrey state msg");
+    state_->set_batt_state(*msg);
+}
---
+++ src/rr_subscriber_joy_impl.cpp
@@ -0,0 +1,14 @@
+#include "rr_common_plugins/rr_subscriber_joy_impl.hpp"
+
+using namespace rr_common_plugins;
+
+/**
+ * @fn callback
+ * 
+ * callback for Joystick.
+ */
+void RrSubscriberJoyImpl::callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+{
+    RCLCPP_DEBUG(logger_, "received message Joy msg");
+    state_->set_joystick(*msg);
+}
